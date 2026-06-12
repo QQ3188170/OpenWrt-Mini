e684672c9d158051633f99517c82944d4d6601fa
rainxi <48948726+QQ3188170@users.noreply.github.com>
2026-06-12 10:59:57 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -2,8 +2,11 @@
 # =======================================================================================================================
 
 # 1. OpenClash
-git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
-# 2. Argon主题
+# 2. 关机插件
+git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
+
+# 3. Argon主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
